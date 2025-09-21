d3ffa822f7002ea557afe45c259001088d5aa4ef
Gabriel Almeida <157758748+GabrielAlmeidaSil@users.noreply.github.com>
2025-09-21 10:05:48 -0300

    Seção Criar Conta finalizada

---
+++ Pages/Shared/_LayoutLogin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GestaoChamadosWeb.Pages.Shared
+{
+    public class _LayoutLoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
