abded5e8c594c66ef69bda66566a6f98819a089a
Gabriel Almeida <157758748+GabrielAlmeidaSil@users.noreply.github.com>
2025-09-28 11:08:45 -0300

    Seção Abrir Chamado & Consultar Finalizada

---
+++ Pages/Tickets/Consult.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GestaoChamadosWeb.Pages.Tickets
+{
+    public class ConsultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Tickets/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GestaoChamadosWeb.Pages.Tickets
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
