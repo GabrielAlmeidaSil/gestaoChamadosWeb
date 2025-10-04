6acbbaccf84703be871bf39670504b49837afee7
Gabriel Almeida <157758748+GabrielAlmeidaSil@users.noreply.github.com>
2025-10-04 17:55:00 -0300

    Seção Notificações finalizada

---
+++ Pages/Account/Notification.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GestaoChamadosWeb.Pages.Account
+{
+    public class NotificationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
